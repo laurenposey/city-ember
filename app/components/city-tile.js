a658f03f1ddb2f414d9b90e5416a606c1d9c410c
Lauren Posey and Noah Yasskin <lauren+noah@>
2016-08-22 10:20:41 -0700

    sort rentals based on price

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.name', 'city.state', function() {
+    return this.get('city.name') + ', ' + this.get('city.state');
+  }),
+
+  sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
 });
